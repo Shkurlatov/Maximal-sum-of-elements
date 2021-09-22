d22cc4b1e270f2aaa97edd797323a1e19285793b
Alexander Shkurlatov <shkurlatov80@gmail.com>
2021-09-22 13:08:47 +1200

    Add project files.

---
+++ SumProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SumProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
